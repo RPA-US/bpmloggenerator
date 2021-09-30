37cb3bd80d31593685d70054c0e4849c02c5cbef
jose.lopez <jose.lopez@iwt2.org>
2021-09-30 08:21:45 +0200

    Changed the structure of the functions in specific files. Updated the mouse coordinate function.

--- plugins/string/random_text_lorem.py
+++ plugins/string/random_text_lorem.py
@@ -60,13 +60,4 @@ def generate_random_entity(words=1):
     return text
 
 
-'''
-Insert one text in another text.
-'''
-def insert_text(text, key):
-    new_text = list(text.split())
-    value = random.randint(0,len(new_text)-1)
-    new_text.insert(value,key)
-    new_text = " ".join([str(x) for x in new_text])
-    return new_text
 
---
+++ tools/generic_utils.py
@@ -0,0 +1,13 @@
+import json
+from plugins.string.random_text_lorem import generate_path, generate_paragraph, generate_sentence, generate_words, generate_DNI,generate_random_entity
+from plugins.list.coordenates_in_range import generate_mouse_position
+from plugins.list.mouse_tipe import generate_mousekeyboard
+from plugins.screenshot.replace_gui_component import generate_screenshot_demo
+from plugins.app.nameapp import generate_app_demo
+'''
+Ver que se hace con las funciones
+'''
+def detect_function(text):
+    f = open('function_trace.json')
+    json_func = json.load(f)
+    return eval(json_func[text])
