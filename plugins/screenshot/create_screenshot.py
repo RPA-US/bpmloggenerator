2626d36fe624b0405c8f74e84742544fa6a68099
jose.lopez <jose.lopez@iwt2.org>
2021-10-15 10:11:58 +0200

    [update]: updated the acumulation variable in the main

--- plugins/screenshot/create_screenshot.py
+++ plugins/screenshot/create_screenshot.py
@@ -1,9 +1,11 @@
 import random
 import time
+import random
+import time
 
 def generate_screenshot_demo(number):
     '''
     Generate a string with an extension jpg
     '''
     # Random number, the timestamp and the extension with a img identification
-    return str(number)+"_"+"_img.png"
+    return str(number)+"_img.png"
