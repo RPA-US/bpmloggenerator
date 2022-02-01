6d8a0ebbdf9e35624a80c6a067a6899570bc03e3
a8081 <amartinezr98@protonmail.com>
2022-02-01 10:47:46 +0100

    [fix]: DB heroku configuration

--- configuration/ci-cd/heroku_settings.py
+++ configuration/ci-cd/heroku_settings.py
@@ -102,6 +102,11 @@ WSGI_APPLICATION = 'agosuirpa.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':         os.environ('DB_NAME'),
+        'HOST':         os.environ('DB_HOST'),
+        'PORT':         os.environ('DB_PORT'),
+        'USER':         os.environ('DB_USER'),
+        'PASSWORD':     os.environ('DB_PASSWORD'),
     }
 }
 
