8f9a2a36fb6d9a559fb4104f7d0e0b5961dcd23f
a8081 <amartinezr98@protonmail.com>
2022-02-01 11:00:00 +0100

    [fix]: secret key automatic generation

--- configuration/ci-cd/heroku_settings.py
+++ configuration/ci-cd/heroku_settings.py
@@ -17,6 +17,7 @@ import environ
 import django_heroku
 import dj_database_url
 import psycopg2
+from django.core.management.utils import get_random_secret_key
 
 
 env = environ.Env()
@@ -35,7 +36,7 @@ ALLOWED_HOSTS = ["agosuirpa.herokuapp.com", "127.0.0.1"]
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECRECT KEY
-SECRET_KEY = env('DJANGO_SECRET_KEY')
+SECRET_KEY = get_random_secret_key()
 
 # Application definition
 INSTALLED_APPS = [
