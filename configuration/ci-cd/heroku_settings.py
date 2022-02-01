97cc5434eae33c8d7bd75f72b620d48f0efd047b
a8081 <amartinezr98@protonmail.com>
2022-02-01 11:10:57 +0100

    [fix]: allowed hosts

--- configuration/ci-cd/heroku_settings.py
+++ configuration/ci-cd/heroku_settings.py
@@ -30,7 +30,7 @@ API_VERSION = 'api/v1/'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["agosuirpa.herokuapp.com", "127.0.0.1"]
+ALLOWED_HOSTS = ['agosuirpa.herokuapp.com']
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
