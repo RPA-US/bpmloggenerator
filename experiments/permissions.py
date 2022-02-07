fc1d6a9efa7175e77d79128aa61036137baff709
a8081 <amartinezr98@protonmail.com>
2022-02-07 13:53:14 +0100

    [feat]: permission function to save screenshots as private storage

---
+++ experiments/permissions.py
@@ -0,0 +1,10 @@
+"""
+Possible functions for the ``PRIVATE_STORAGE_AUTH_FUNCTION`` setting.
+"""
+import django
+import experiments.models as Experiment_model
+
+def allow_staff(private_file):
+    request = private_file.request
+    exp = Experiment_model.Product.objects.filter(screenshots=private_file.relative_name, user=request.user)
+    return exp and exp.exists()
