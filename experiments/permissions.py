5fc5036d9c6e10bfe3c928afa875c0d33676f691
jose.lopez <jose.lopez@iwt2.org>
2022-03-07 12:50:17 +0100

    [fix]: image permisson acces fixed

--- experiments/permissions.py
+++ experiments/permissions.py
@@ -2,13 +2,18 @@
 Possible functions for the ``PRIVATE_STORAGE_AUTH_FUNCTION`` setting.
 """
 import experiments.models as Experiments_model
+from users.models import CustomUser
+from rest_framework.authtoken.models import Token
+
 
 def allow_staff(private_file):
     res = False
     request = private_file.request
-    screenshot = Experiments_model.Screenshot.objects.filter(relative_path=private_file.relative_name)
+    token = request.headers._store['authorization'][1].split(" ")[1]
+    user = Token.objects.filter(key = token)[0].user
+    screenshot=Experiments_model.Screenshot.objects.filter(
+        relative_path=private_file.relative_name)
     if screenshot:
-        screenshot = screenshot[0]
-        user = screenshot.experiment.user_id
-        res = user == request.user.id
+        screenshot=screenshot[0]
+        res=user == screenshot.experiment.user
     return res
