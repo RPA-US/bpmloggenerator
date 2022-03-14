131acf385f67d4ce8363d720edd6fb836c1bd1d6
jose.lopez <jose.lopez@iwt2.org>
2022-03-14 18:58:07 +0100

    [patch]: path for screenshot args

--- plugins/screenshot/create_screenshot.py
+++ plugins/screenshot/create_screenshot.py
@@ -4,13 +4,24 @@ from experiments.models import Variations
 from agosuirpa.generic_utils import detect_function
 from agosuirpa.system_configuration import sep
 
+
 def manage_dependency(experiment, name, arguments, j, case, scenario, activity, variant):
     if "args_dependency" in j:
-        dependant_row = Variations.objects.get(experiment=experiment, case_id=case, scenario=scenario, activity=j["args_dependency"]["Activity"], case_variation_id=j["args_dependency"]["id"], variant=j["args_dependency"]["V"])
-        arguments.append(dependant_row.gui_element)                                
+        dependant_row = Variations.objects.get(experiment=experiment, case_id=case, scenario=scenario,
+                                               activity=j["args_dependency"]["Activity"], case_variation_id=j["args_dependency"]["id"], variant=j["args_dependency"]["V"])
+        arguments.append(dependant_row.gui_element)
     image_element = util.detect_function(name)(arguments)
     if type(image_element) == str:
-        Variations.objects.create(experiment=experiment, case_id=case, scenario=scenario, case_variation_id=j["id"], activity=activity, variant=variant, function_name=name, gui_element=image_element)
+        Variations.objects.create(experiment=experiment, case_id=case, scenario=scenario,
+                                  case_variation_id=j["id"], activity=activity, variant=variant, function_name=name, gui_element=image_element)
+
+
+def args_dictionary_to_list(list_dict):
+    argsList = []
+    for i in list_dict:
+        argsList.append(list_dict[i])
+    return argsList
+
 
 def generate_capture(experiment, columns_ui, columns, element, acu, case, generate_path, attr, activity, variant):
     '''
@@ -19,13 +30,14 @@ def generate_capture(experiment, columns_ui, columns, element, acu, case, genera
         dict: json with the trace
         case: number of the case
         variante: if use the initial value or the generate
-    '''    
+    '''
     # actual_path = os.getcwd()
     capture_path = element["initValue"]
     args_tmp = element["args"]
-    args = [generate_path,acu]
+    args = [generate_path, acu]
     #new_image = generate_screenshot_demo(args)
-    new_image = detect_function(experiment.screenshot_name_generation_function)(args)
+    new_image = detect_function(
+        experiment.screenshot_name_generation_function)(args)
     try:
         for i in columns_ui:
             try:
@@ -40,24 +52,25 @@ def generate_capture(experiment, columns_ui, columns, element, acu, case, genera
                         if element is not None:
                             coordinates = j["coordinates"]
                             name = j["name"]
-                            args = j["args"]
+                            args = args_dictionary_to_list(j["args"])
                             if not sep in new_image:
                                 image_path_to_save = generate_path + new_image
                             else:
                                 image_path_to_save = new_image
-                                
+
                             # Check if there are previous variations applied to the original image and substitute the original image path by the modified one
                             if os.path.exists(image_path_to_save):
-                                capture_path=image_path_to_save
-                                
+                                capture_path = image_path_to_save
+
                             arguments.append(args)
                             arguments.append(image_path_to_save)
                             arguments.append(capture_path)
                             arguments.append(coordinates)
-                            
-                            manage_dependency(experiment, name, arguments, j, case, 0, activity, variant)
+
+                            manage_dependency(
+                                experiment, name, arguments, j, case, 0, activity, variant)
                         else:
-                            new_image=""
+                            new_image = ""
                         arguments = []
             except Exception as e:
                 # print("Unexpected error: " + str(e)) // TODO
@@ -68,7 +81,8 @@ def generate_capture(experiment, columns_ui, columns, element, acu, case, genera
         new_image = ""
     return new_image
 
-def generate_scenario_capture(experiment,element,case,generate_path,activity,variant,new_image,scenario,attachments_path):
+
+def generate_scenario_capture(experiment, element, case, generate_path, activity, variant, new_image, scenario, attachments_path):
     '''
     Generate row reading the json
     args:
@@ -78,7 +92,7 @@ def generate_scenario_capture(experiment,element,case,generate_path,activity,var
     '''
     capture_path = attachments_path + sep + element["initValue"]
     json_args = element["args"]
-    args = [generate_path,case]
+    args = [generate_path, case]
 
     arguments = []
     for gui_component_label in json_args:
@@ -91,19 +105,20 @@ def generate_scenario_capture(experiment,element,case,generate_path,activity,var
                     image_path_to_save = generate_path + new_image
                 else:
                     image_path_to_save = new_image
-                    
+
                 # Check if there are previous variations applied to the original image and substitute the original image path by the modified one
                 if os.path.exists(image_path_to_save):
-                    capture_path=image_path_to_save
-                    
+                    capture_path = image_path_to_save
+
                 arguments.append(args)
                 arguments.append(image_path_to_save)
                 arguments.append(capture_path)
                 arguments.append(coordinates)
-                
-                manage_dependency(experiment, name, arguments, variation_conf, case, scenario, activity, variant)
+
+                manage_dependency(experiment, name, arguments,
+                                  variation_conf, case, scenario, activity, variant)
             else:
-                new_image=""
+                new_image = ""
             arguments = []
-    
+
     return new_image
