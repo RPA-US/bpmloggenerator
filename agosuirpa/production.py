d006d0753f1a4e2641a76e3364ed8b953376873c
a8081 <amartinezr98@protonmail.com>
2022-03-17 11:37:35 +0100

    [fix]: django settings configured to depends on environment variable

--- agosuirpa/production.py
+++ agosuirpa/production.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 import sys
 import environ
+from django.core.management.utils import get_random_secret_key
 
 # Initialise environment variables
 env = environ.Env()
@@ -22,7 +23,7 @@ environ.Env.read_env()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['150.214.188.176']
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,7 +38,7 @@ DB_PASSWORD =   env('DB_PASSWORD')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('DJANGO_SECRET_KEY')
+SECRET_KEY = get_random_secret_key()
 
 # Application definition
 INSTALLED_APPS = [
