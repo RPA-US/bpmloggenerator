9f543188a95ef9b0df5a30850a5f46fe964a9613
jose.lopez <jose.lopez@iwt2.org>
2022-04-22 10:47:41 +0200

    [fix]: fixed the saved screenshot in updated experiment

--- experiments/views.py
+++ experiments/views.py
@@ -137,15 +137,18 @@ class ExperimentUpdateView(generics.RetrieveUpdateDestroyAPIView):
         return Response(ExperimentSerializer(experiment).data)
 
     def put(self, request, id, *args, **kwars):
+
         try:
             user = CustomUser.objects.get(id=self.request.user.id)
             experiment = get_object_or_404(Experiment, user=user.id, id=id)
         except:
             return Response({"message": "No user found"}, status=status.HTTP_404_NOT_FOUND)
-        st = status.HTTP_200_OK
-        msg = "Experiment updated"
+
+        msg = 'ok, created'
+        st = status.HTTP_201_CREATED
 
         execute_mode = request.data.get('execute_mode')
+
         try:
             if execute_mode:
                 if request.data.get('number_scenarios') and int(request.data.get('number_scenarios')) > 0 and not ('scenarios_conf' in request.data):
@@ -164,6 +167,7 @@ class ExperimentUpdateView(generics.RetrieveUpdateDestroyAPIView):
                 for data in ['name', 'special_colnames', 'screenshot_name_generation_function']:
                     if not data in request.data:
                         return Response({"message": "POST experiment saving - Incomplete data: " + data + " not included"}, status=status.HTTP_400_BAD_REQUEST)
+
             experiment.size_balance = json_attributes_load(
                 request.data.get('size_balance'))
             experiment.name = request.data.get('name')
@@ -176,22 +180,20 @@ class ExperimentUpdateView(generics.RetrieveUpdateDestroyAPIView):
                 'scenarios_conf')) if request.data.get('scenarios_conf') else None
             experiment.special_colnames = json_attributes_load(
                 request.data.get('special_colnames'))
+            experiment.screenshots = request.data.get('screenshots')
             experiment.screenshot_name_generation_function = request.data.get(
                 'screenshot_name_generation_function')
             experiment.last_edition = datetime.datetime.now(tz=timezone.utc)
+
+            experiment.save()   
+            
             if 'screenshots' in request.data and experiment.screenshots != request.data.get('screenshots'):
-                experiment.screenshots = request.data.get('screenshots')
                 path_without_fileextension = upload_mockups(
                     'privatefiles'+sep+experiment.screenshots.name)
                 experiment.screenshots_path = path_without_fileextension
                 associate_screenshots_files(experiment)
 
-            experiment.save()   
             if execute_mode:
-                if (experiment.number_scenarios > 0 and not experiment.scenarios_conf):
-                    raise AssertionError(
-                        "Scenarios variability configuration must be present if number of scenarios greater than 0")
-
                 experiment.execution_start = datetime.datetime.now(
                     tz=timezone.utc)
                 experiment.foldername = execute_experiment(experiment)
@@ -203,13 +205,14 @@ class ExperimentUpdateView(generics.RetrieveUpdateDestroyAPIView):
             elif request.data.get('status') != ExperimentStatusChoice.PR.value:
                 experiment.status = ExperimentStatusChoice.SA.value
             experiment.save()
+
+            response_content = {"message": msg,
+                "id": experiment.id, "status": experiment.status}
+
         except Exception as e:
             msg = 'Some of atributes are invalid: ' + str(e)
             st = status.HTTP_422_UNPROCESSABLE_ENTITY                             
-        except Exception as e:
-            msg = 'Some of atributes are invalid: ' + str(e)
-            st = status.HTTP_422_UNPROCESSABLE_ENTITY
-        response_content = {"message": msg}
+            response_content = {"message": msg}
         return Response(response_content, status=st)
 
     def delete(self, request, id, *args, **kwars):
