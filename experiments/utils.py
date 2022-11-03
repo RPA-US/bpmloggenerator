1daab9f98dcfbf5c85d8743792910faa329eb033
a8081 <antoniommrr@pm.me>
2022-11-03 13:27:39 +0100

    [fix]: UI LOGS FOLDERNAME environment variable

--- experiments/utils.py
+++ experiments/utils.py
@@ -24,7 +24,7 @@ def upload_mockups(zip_path):
 def compress_experiment(path, file_name):
     folder_path = split_name_system(path)
     zip_file = folder_path + sep + file_name 
-    abs_path = os.path.abspath(folder_path + sep + ui_logs_foldername)
+    abs_path = os.path.abspath(folder_path + sep + UI_LOGS_FOLDERNAME)
     zip_path = zip_file + ".zip"
     if not os.path.exists(zip_path):
         zip_path = shutil.make_archive(zip_file, 'zip', abs_path)
