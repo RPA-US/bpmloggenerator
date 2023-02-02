851a17afe3df7012c0cc5d028bf55e0d24f55318
Antonio <antoniommrr@gmail.com>
2023-02-02 19:53:52 +0100

    [feat]: separator as a settings attribute, not auto-detected

--- agosuirpa/production.py
+++ agosuirpa/production.py
@@ -274,11 +274,11 @@ ACCOUNT_LOGIN_ATTEMPTS_TIMEOUT = 86400
 operating_system =sys.platform
 print("Operating system detected: " + operating_system)
 # Element specification filename and path separator (depends on OS)
-if "windows" in operating_system:
-    sep = "\\"
-    element_trace = "configuration"+sep+"element_trace.json"
-else:
-    sep = "/"
-    element_trace = "configuration"+sep+"element_trace_linux.json"
+# if "win" in operating_system:
+#     sep = "\\"
+#     element_trace = "configuration"+sep+"element_trace.json"
+# else:
+sep = "/"
+element_trace = "configuration"+sep+"element_trace_linux.json"
 # Function specification filename
 function_trace = "configuration"+sep+"function_trace.json"
